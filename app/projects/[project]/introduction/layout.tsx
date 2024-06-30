933edc62d0fbdb1d722a9b0351a14f1e636a0d46
Xuan Hung <xuanhungptithcm@gmail.com>
2024-07-01 03:53:51 +0700

    fix get milestone

---
+++ app/projects/[project]/introduction/layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useMilestoneStore } from "@/store/useMilestoneStore";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+
+export default function IntroductionLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { step, milestone, project } = useParams<{
+    milestone: string;
+    step: string;
+    project: string;
+  }>();
+  const { projectNameSelected, setProjectNameSelected } = useMilestoneStore();
+
+  useEffect(() => {
+    setProjectNameSelected(project || "", milestone, step);
+  }, [step, milestone, projectNameSelected]);
+
+  return <section>{children}</section>;
+}
