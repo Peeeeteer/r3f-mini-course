a43a5cd4393be7abc17a341bb1955c39addea423
fullstack-guy <andriihryhoriev1@gmail.com>
2024-07-12 13:54:16 +0800

    Implement updating user info based on the Stripe paid status

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+import { Stripe } from "stripe";
+
+import { createClient } from "@/utils/supabase/server";
+
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string);
+
+// Stripe will give you a webhook secret when setting up webhooks.
+// We'll get this later and add it to the .env.local file when testing
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
+
+export async function POST(req: Request) {
+  const payload = await req.text(); // Get raw data of body
+  const signature = req.headers.get("stripe-signature");
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(payload, signature!, webhookSecret);
+
+    switch (event?.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object as Stripe.PaymentIntent;
+        const userEmail = paymentIntent.receipt_email;
+        const projectId = paymentIntent.metadata?.projectId
+
+        if (userEmail && projectId) {
+          const supabase = createClient()
+          const { error } = await supabase.rpc('update_user_projects', {
+            u_email: userEmail,
+            p_id: projectId
+          })
+
+          if (error) return NextResponse.json({ message: error.message }, { status: 400 });
+          console.log('user data updated successfully')
+
+          return NextResponse.json({ message: 'it works' }, { status: 200 });
+        }
+
+        
+        return NextResponse.json({ message: `it works but can't update user info` }, { status: 500 });
+      default:
+        // other events that we don't handle
+        break;
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      return NextResponse.json({ message: err.message }, { status: 400 });
+    }
+  }
+
+  return NextResponse.json({ received: true });
+}
--- app/projects/[project]/[milestone]/layout.tsx
+++ app/projects/[project]/[milestone]/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { usePathname, useRouter, useParams } from "next/navigation";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from 'axios';
 
@@ -10,6 +10,14 @@ import Stepper from "@/components/Stepper";
 import FooterProcess from "@/containers/projects/components/FooterProcess";
 import { useAuthContext } from "@/contexts/AuthContext";
 import { useMilestoneStore } from "@/store/useMilestoneStore";
+import { createClient } from '@/utils/supabase/client'
+
+type ProjectInfoType = {
+  id: string
+  price_id: string
+  is_free: boolean
+  is_paid: boolean
+}
 
 export default function MilestoneLayout({
   children,
@@ -17,68 +25,114 @@ export default function MilestoneLayout({
   children: React.ReactNode;
 }>) {
   const navigation = useRouter();
-  const params = usePathname();
+  const pathname = usePathname();
+  const params = useParams()
+  const supabase = createClient();
 
   const { milestone } = useMilestoneStore();
   const { isAuthenticated } = useAuthContext()
   const { currentHint = 0, hints = [] } = milestone || {};
+  const projectParam = params.project
 
-  // need to add logic for checking if the user is paid or not
-  const isPaid = false;
+  const [projectInfo, setProjectInfo] = useState<ProjectInfoType | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getProjectInfo = useCallback(async () => {
+    setIsLoading(true)
+    const project = Array.isArray(projectParam) ? projectParam[0] : projectParam
+    const { data, error } = await supabase.rpc('get_project_info', {
+      p_name: project
+    })
+    setIsLoading(false)
+
+    if (error) {
+      console.error('An error occurred while getting project infor from Supabase')
+    }
+
+    setProjectInfo(data[0])
+  }, [projectParam])
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getProjectInfo()
+    }
+  }, [isAuthenticated])
+
+  const isViewable = useMemo(() => {
+    if (projectInfo?.is_free || projectInfo?.is_paid) return true
+    return false
+  }, [projectInfo])
 
   const handlePurchaseClick = useCallback(async () => {
     if (isAuthenticated) {
-      const stripe = await loadStripe(
-        process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
-      );
-      if (!stripe) {
-        return;
-      }
-      try {
-        const response = await axios.post('/api/stripe/checkout', {
-          priceId: 'price_1PP4DJGLPOPU9PEJ67Zo2mpJ'
-        });
-        const data = response.data;
-        if (!data.ok) throw new Error('Something went wrong');
-        await stripe.redirectToCheckout({
-          sessionId: data.result.id
-        });
-      } catch (error) {
-        console.log(error);
+      if (projectInfo) {
+        const stripe = await loadStripe(
+          process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
+        );
+        if (!stripe) {
+          return;
+        }
+        try {
+          const response = await axios.post('/api/stripe/checkout', {
+            priceId: projectInfo.price_id,
+            projectId: projectInfo.id
+          });
+          const data = response.data;
+          if (!data.ok) throw new Error('Something went wrong');
+          await stripe.redirectToCheckout({
+            sessionId: data.result.id
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      } {
+        getProjectInfo()
       }
     } else {
       navigation.push('/auth/login')
     }
-  }, [isAuthenticated])
+  }, [isAuthenticated, getProjectInfo, projectInfo])
 
   return (
-    isPaid ? <div className="flex flex-col h-full">
-      < div className="mb-6" >
-        <Stepper
-          activeStep={currentHint || 0}
-          onChangeStep={(step) => {
-            if (!milestone) return;
-            const urlSplit = params.split("/");
-            urlSplit.shift();
-            urlSplit.pop();
-            urlSplit.push(step.toLocaleLowerCase());
-            const newUrl = urlSplit.join("/");
-            navigation.push("/" + newUrl);
-          }}
-          steps={hints.map((hint) => hint?.label || "")}
-        ></Stepper>
-      </div >
-      <div className="h-[calc(100dvh-290px)] overflow-y-auto scrollable -mr-4 pr-4">{children}</div>
-      <FooterProcess />
-    </div >
+    isAuthenticated ? <>
+      {
+        isLoading ? <p className="text-white">Loading</p> :
+          (
+            isViewable ?
+              <div className="flex flex-col h-full">
+                < div className="mb-6" >
+                  <Stepper
+                    activeStep={currentHint || 0}
+                    onChangeStep={(step) => {
+                      if (!milestone) return;
+                      const urlSplit = pathname.split("/");
+                      urlSplit.shift();
+                      urlSplit.pop();
+                      urlSplit.push(step.toLocaleLowerCase());
+                      const newUrl = urlSplit.join("/");
+                      navigation.push("/" + newUrl);
+                    }}
+                    steps={hints.map((hint) => hint?.label || "")}
+                  ></Stepper>
+                </div >
+                <div className="h-[calc(100dvh-290px)] overflow-y-auto scrollable -mr-4 pr-4">{children}</div>
+                <FooterProcess />
+              </div >
+              :
+              <div className="w-full h-full flex items-center justify-center">
+                <PrimaryButton
+                  text="Purchase this project"
+                  type="button"
+                  className="rounded-md text-white border border-[#FFFFFF0F] bg-[#FFFFFF14]"
+                  onClick={handlePurchaseClick}
+                />
+              </div>
+          )
+      }
+    </>
       :
       <div className="w-full h-full flex items-center justify-center">
-        <PrimaryButton
-          text="Purchase this project"
-          type="button"
-          className="rounded-md text-white border border-[#FFFFFF0F] bg-[#FFFFFF14]"
-          onClick={handlePurchaseClick}
-        />
+        <p className="text-lg font-semibold">You need to log in to see this tutorial</p>
       </div>
   );
 }
